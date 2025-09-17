b602fde7089a3214c6659a77d614480856a8fea5
Anselmo Martinoli <anselmomartinoli@libero.it>
2025-09-17 17:09:07 +0200

    Punto di ripristino 10: Interfaccia interattiva funzionante

--- lib/table_page.dart
+++ lib/table_page.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
 
-class TablePage extends StatelessWidget {
+class TablePage extends StatefulWidget {
   final int tableNumber;
-  
+
   const TablePage({super.key, required this.tableNumber});
+
+  @override
+  State<TablePage> createState() => _TablePageState();
+}
+
+class _TablePageState extends State<TablePage> {
+  // Mappa per tenere traccia degli articoli ordinati e delle loro quantità
+  final Map<String, int> _orderedItems = {};
+
+  // Lista statica di articoli del menu
+  final List<String> menuItems = [
+    'Pizza Margherita',
+    'Spaghetti alla Carbonara',
+    'Risotto ai funghi',
+    'Acqua naturale',
+    'Coca-Cola',
+    'Birra'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Ordinazione Tavolo $tableNumber'),
+        title: Text('Ordinazione Tavolo ${widget.tableNumber}'),
         backgroundColor: Colors.blue,
       ),
-      body: const Center(
-        child: Text('Dettagli dell\'ordinazione'),
+      body: ListView.builder(
+        itemCount: menuItems.length,
+        itemBuilder: (context, index) {
+          final item = menuItems[index];
+          final quantity = _orderedItems[item] ?? 0;
+          return ListTile(
+            title: Text(item),
+            trailing: quantity > 0
+                ? Text('$quantity', style: Theme.of(context).textTheme.headlineSmall)
+                : null,
+            onTap: () {
+              setState(() {
+                _orderedItems[item] = quantity + 1;
+              });
+              print('Ordinazione per il tavolo ${widget.tableNumber}: $_orderedItems');
+            },
+          );
+        },
       ),
     );
   }

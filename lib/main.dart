d50805662df0c3fd4c047696a3851079b6716a69
Anselmo Martinoli <anselmomartinoli@libero.it>
2025-09-17 16:35:35 +0200

    Punto di ripristino 6: Struttura del progetto funzionante

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ordinazione/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Ciao mondo'),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }

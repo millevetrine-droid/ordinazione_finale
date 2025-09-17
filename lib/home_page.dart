e1b766f0cc64ec4801453cbedf220bb75858b85c
Anselmo Martinoli <anselmomartinoli@libero.it>
2025-09-17 16:42:51 +0200

    Punto di ripristino 7: Interfaccia utente di base

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,9 +5,37 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ordinazione App'),
+        backgroundColor: Colors.blue,
+      ),
       body: Center(
-        child: Text('Ciao mondo!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Azione per il pulsante 1
+              },
+              child: const Text('Tavolo 1'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Azione per il pulsante 2
+              },
+              child: const Text('Tavolo 2'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Azione per il pulsante 3
+              },
+              child: const Text('Tavolo 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
